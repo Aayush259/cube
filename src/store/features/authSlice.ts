2356942e3030c79424dab35dcf937d3d984adb34
Aayush <aayushk4980@gmail.com>
2024-10-13 00:51:44 +0530

    Created store and authSlice

---
+++ src/store/features/authSlice.ts
@@ -0,0 +1,26 @@
+import { AuthState } from "@/interfaces/interface";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState: AuthState = {
+  status: false,
+  userData: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.status = true;
+      state.userData = action.payload;
+    },
+    logout: (state) => {
+      state.status = false;
+      state.userData = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;
